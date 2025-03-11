b1c279b8403ed225982e404a7aa0f288a488a15f
Alanshan0324 <72635879+Alanshan0324@users.noreply.github.com>
2025-03-11 11:19:34 +0100

    Add logout button at bartender_frontend

--- bartender_frontend.py
+++ bartender_frontend.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 from tkinter import messagebox
 from bartender_backend import BartenderController, Employee
-
+import subprocess
+import sys
 
 class BartenderFrontend:
     def __init__(self, controller):
@@ -134,10 +135,22 @@ class BartenderFrontend:
 
         self.status_label = tk.Label(self.control_frame, text="", fg="blue")
         self.status_label.grid(row=row, column=0, sticky="w", padx=5, pady=5)
+        tk.Button(self.control_frame, text="Log out", command=self.logout) \
+            .grid(row=row, column=0, sticky="ew", padx=5, pady=2);
+        row += 1
 
         self.refresh_products()
         self.refresh_order()
-
+    def logout(self):
+        """登出並回到登入介面"""
+        confirm = messagebox.askyesno("Logout", "Are you sure you want to logout?")
+        if confirm:
+            
+   
+            self.root.quit()  # 結束 Tkinter 事件循環
+            self.root.destroy()  # 銷毀 Tkinter 主視窗
+            subprocess.Popen([sys.executable, "login_interface.py"], start_new_session=True)  # 啟動新視窗
+            
     def refresh_products(self):
         self.product_listbox.delete(0, tk.END)
         products = [self.controller.get_product(pid) for pid in self.controller.get_product_ids()]
